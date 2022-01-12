d5b5c0eca2be92e4a21f6105541807a83fcc4999
Пользователь <aytacrustmli93@gmail.com>
2022-01-12 12:11:12 +0400

    12.01.2022

---
+++ homework/Program.cs
@@ -0,0 +1,10 @@
+﻿using homework;
+
+
+Product product1 = new Product();
+product1.Name = "A70";
+product1.Price = 800;
+product1.Model = "Samsung";
+
+Console.WriteLine(product1.Hash());
+Console.WriteLine(product1.Hash2());
---
+++ homework/product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    public class Product
+    {
+        public int Price;
+        public string Name;
+        public string Model;
+
+
+        public string Hash()
+        {
+            static void SEOLink(string text)
+            {
+                var seo = text.ToLower().Replace(" ", " -")
+                              .Replace("ə", "e")
+                              .Replace("ğ", "g")
+                              .Replace("ç", "c");
+
+            }
+            return Name + " " + Model + " " + Price;
+        }
+        public int Hash2()
+        {
+            return Price + Price * 18 / 100;
+        }
+
+    }
+}
